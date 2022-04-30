dd7c94166bb0b0b8f973aad138c65c1d2ba7529a
Seungwoo Yoo <ryuryu10@naver.com>
2022-04-30 15:21:02 +0900

    overlay

--- main.py
+++ main.py
@@ -34,10 +34,10 @@ overlay_font = pygame.font.SysFont(None, 30)
 hwnd = pygame.display.get_wm_info()["window"]
 win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
-pos_x1 = 200
-pos_y1 = 200
-pos_x2 = 200
-pos_y2 = 200
+pos_x1 = 699.2
+pos_y1 = 52.7
+pos_x2 = 7.5
+pos_y2 = 8.9
 switched = 1
 while hack_running:
     for event in pygame.event.get():
@@ -54,44 +54,21 @@ while hack_running:
     x_text = overlay_font.render("X: ", True, (255, 0, 0))
     y_text = overlay_font.render("Y: ", True, (255, 0, 0))
     z_text = overlay_font.render("Z: ", True, (255, 0, 0))
-    level_area = pygame.draw.rect(screen,(255,255,255),(94.7,46.4,36.3,27.6),1)
-    level_point_area = pygame.draw.rect(screen,(255,255,255),(131.3,45.3,52.6,30.5),1)
+    level_area = pygame.draw.rect(screen,(0,0,0),(94.7,46.4,36.3,27.6),1)
+    level_point_area = pygame.draw.rect(screen,(0,0,0),(131.3,45.3,52.6,30.5),1)
     gem_area = pygame.draw.rect(screen,(255,255,255),(288.8,43.4,60.3,32.6),1)
     coin_area = pygame.draw.rect(screen,(255,255,255),(427.7,44.3,111.4,30.2),1)
-
-
-
-    a = pygame.draw.rect(screen,(255,255,255),(pos_x1,pos_y1,pos_x2,pos_y2),1)
-    print(pos_x1,pos_y1,pos_x2,pos_y2)
-    if keyboard.is_pressed("q"):
-        if switched == 1:
-            switched = 0
-        else :
-            switched = 1
-
-    if keyboard.is_pressed("a"):
-        if switched == 1:
-            pos_x1 -= 0.1
-        else:
-            pos_x2 -= 0.1
-
-    if keyboard.is_pressed("d"):
-        if switched == 1:
-            pos_x1 += 0.1
-        else:
-            pos_x2 += 0.1
-
-    if keyboard.is_pressed("w"):
-        if switched == 1:
-            pos_y1 -= 0.1
-        else:
-            pos_y2 -= 0.1
-
-    if keyboard.is_pressed("s"):
-        if switched == 1:
-            pos_y1 += 0.1
-        else:
-            pos_y2 += 0.1
+    heart_1 = pygame.draw.rect(screen,(255,255,255),(686.2,52.7,7.5,8.9),1)
+    heart_2 = pygame.draw.rect(screen,(255,255,255),(699.2,52.7,7.5,8.9),1)
+    heart_3 = pygame.draw.rect(screen,(255,255,255),(711.9,52.7,7.5,8.9),1)
+    heart_4 = pygame.draw.rect(screen,(255,255,255),(723,52.7,7.5,8.9),1)
+    heart_5 = pygame.draw.rect(screen,(255,255,255),(736.2,52.7,7.5,8.9),1)
+    start_button_1 = pygame.draw.rect(screen,(255,255,255),(673.4,507.4,148.2,54.2),1)
+    start_button_2 = pygame.draw.rect(screen,(255,255,255),(633.1,485.4,146.8,49.3),1)
+    end_coin = pygame.draw.rect(screen,(255,255,255),(751.6,316.5,142.5,61.3),1)
+    end_button = pygame.draw.rect(screen,(255,255,255),(319.4,503.2,93.5,60.6),1)
+    end_box = pygame.draw.rect(screen,(255,255,255),(428.9,506.2,157.3,51),1)
+    end_box_ok = pygame.draw.rect(screen,(255,255,255),(462.9,508.1,80,47.7),1)
 
     #OVERLAY DRAWS:
     screen.blit(local_text, (15,40))
